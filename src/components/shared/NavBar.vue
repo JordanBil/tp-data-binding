2df9772e393635b9d48b480f9d137e8c2b4890d2
jordan <jordan-billon@hotmail.fr>
2024-12-04 15:35:24 +0100

    first commit

---
+++ src/components/shared/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link class="nav-link active" aria-current="page" to="/home">Home</router-link>
+  <router-link class="nav-link active" aria-current="page" to="/tp">TP</router-link>
+</template>
+  
+<script lang="js">
+import { RouterLink } from 'vue-router'
+</script>
+  
+  <style>
+  /* Si nécessaire, ajoute des styles ici */
+  </style>
+  
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            alias: "/home",
+            name: "Home",
+            component: () => import("../views/HomeView.vue")
+        },
+        {
+            path: "/tp",
+            alias: "/tp",
+            name: "Tp",
+            component: () => import("../views/Tp.vue")
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'NotFound',
+            component: () => import('../views/NotFoundView.vue')
+        },
+    ]
+});
+
+export default router;
